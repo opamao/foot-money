e4f2ca57fb08b3f2b38925c81809739be822fb59
opamao <theodoreyapi@gmail.com>
2024-09-12 01:35:35 +0000

    design news

--- lib/src/features/actualite/pages/actualite_page.dart
+++ lib/src/features/actualite/pages/actualite_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:footmoney/main.dart';
+import 'package:footmoney/src/themes/themes.dart';
+import 'package:gap/gap.dart';
+import 'package:sizer/sizer.dart';
 
 class ActualitePage extends StatefulWidget {
   const ActualitePage({super.key});
@@ -8,8 +12,92 @@ class ActualitePage extends StatefulWidget {
 }
 
 class _ActualitePageState extends State<ActualitePage> {
+  final List<Map<String, String>> newsArticles = [
+    {
+      "title":
+          "Cameroun-Namibie : la Fecafoot dévoile les compos, le staff bloqué",
+      "content": "Détails de l'article 1",
+      "image": "assets/images/actualite1.jpeg"
+    },
+    {
+      "title": "Sénégal : El-Hadji Diouf réagit aux sifflets",
+      "content": "Détails de l'article 2",
+      "image": "assets/images/actu2.jpeg"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(4.w),
+          child: Column(
+            children: [
+              Card(
+                elevation: 5,
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(15)),
+                      child: Image.asset(
+                        newsArticles[0]["image"]!,
+                        height: 219,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(2.w),
+                      child: Text(
+                        newsArticles[0]["title"]!,
+                        style: TextStyle(
+                          color: appBlack,
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Gap(3.h),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: newsArticles.length - 1,
+                  itemBuilder: (context, index) {
+                    final article = newsArticles[index + 1];
+                    return Card(
+                      elevation: 5,
+                      child: Row(
+                        children: [
+                          // print('Afficher les détails de ${article["title"]}');
+                          ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(15)),
+                            child: Image.asset(article["image"]!),
+                          ),
+                          Gap(2.w),
+                          Expanded(
+                            child: Text(
+                              article["title"]!,
+                              style: TextStyle(
+                                color: appBlack,
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
